96ec03e500aa8818ad26d75fdf408e31d30d5072
ShavsGR <garodjavier@gmail.com>
2026-08-14 18:05:38 +0200

    feat: Infrastructure layer

---
+++ src/PriceTracker.Infrastructure/Persistence/Configurations/AlertConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceTracker.Infrastructure.Persistence.Configurations
+{
+    public class AlertConfiguration : IEntityTypeConfiguration<PriceAlert>
+    {
+        public void Configure(EntityTypeBuilder<PriceAlert> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.ProductId).IsRequired();
+            builder.Property(a => a.UserId).IsRequired();
+
+            builder.ComplexProperty(a => a.TargetPrice, precioBuilder =>
+            {
+                precioBuilder.Property(p => p.PriceValue)
+                    .HasColumnName("TargetPrice_Value")
+                    .HasPrecision(18, 2)
+                    .IsRequired();
+
+                precioBuilder.Property(p => p.Currency)
+                    .HasColumnName("TargetPrice_Currency")
+                    .HasMaxLength(3)
+                    .IsRequired();
+            });
+
+            // Ignore domain events in the table persistence
+            builder.Ignore(a => a.DomainEvents);
+        }
+    }
+}
---
+++ src/PriceTracker.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceTracker.Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.ProductURL)
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            // The Value Object "Price" map as a integrated property (ComplexProperty)
+            builder.ComplexProperty(p => p.CurrentPrice, precioBuilder =>
+            {
+                precioBuilder.Property(p => p.PriceValue)
+                    .HasColumnName("CurrentPrice_Value")
+                    .HasPrecision(18, 2)
+                    .IsRequired();
+
+                precioBuilder.Property(p => p.Currency)
+                    .HasColumnName("CurrentPrice_Currency")
+                    .HasMaxLength(3)
+                    .IsRequired();
+            });
+        }
+    }
+}
